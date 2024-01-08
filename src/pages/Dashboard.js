c6d0114a4861b56cd9e93dfad1543d0f94ff815b
Joshua Kim <joskim24@bergen.org>
2024-01-08 09:47:07 -0500

    Finished conversion of dashboard html page to React component.

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,13 +1,79 @@
 import React from 'react';
-import './App.css';
+import './Dashboard.css';
 
 function Dashboard() {
   return (
     <div>
-      <header>
-        <h1>Dashboard -- Login Successful</h1>
-        
-      </header>
+      <div class="header">
+  <h2>Debate Tournament Dashboard</h2>
+</div>
+
+<div class="navbar">
+  <a href="dashboard.html" class="active">Dashboard</a>
+  <a href="leader.html">Leaderboard</a>
+  <a href="#admin">Welcome, Admin</a>
+  <a href="/" style={{ float: "right" }}>
+    Log Out
+  </a>
+
+</div>
+
+<div class="main">
+  <div class="tab">
+    <button class="tablinks active">Junior Varsity</button>
+    <button class="tablinks">Varsity</button>
+  </div>
+  <div class="tournament-card">
+    <h4>Tournament 1</h4>
+    <p>Date: 10/23/2023</p>
+    <p>Schools attending: 10</p>
+    <p>Location: Bergen County Academies</p>
+    <p>Judges: 30</p>
+    <button onclick="window.location='edit_tournament.html'">Edit</button>
+  
+  </div>
+
+  <div class="tournament-card">
+    <h4>Tournament 2</h4>
+    <p>Date: 11/26/2023</p>
+    <p>Schools attending: 9</p>
+    <p>Location: Fort Lee</p>
+    <p>Judges: 27</p>
+    <button onclick="window.location='edit_tournament.html'">Edit</button>
+
+  </div>
+  <div class="tournament-card">
+    <h4>Tournament 3</h4>
+    <p>Date: 12/8/2023</p>
+    <p>Schools attending: 11</p>
+    <p>Location: Bergen County Academies</p>
+    <p>Judges: 30</p>
+    <button onclick="window.location='edit_tournament.html'">Edit</button>
+
+  </div>
+  <div class="tournament-card">
+    <h4>Tournament 4</h4>
+    <p>Date: 1/6/2024</p>
+    <p>Schools attending: 7</p>
+    <p>Location: Demarest</p>
+    <p>Judges: 24</p>
+    <button onclick="window.location='edit_tournament.html'">Edit</button>
+
+  </div>
+  <div class="tournament-card">
+    <h4>Tournament 5</h4>
+    <p>Date: 2/12/2024</p>
+    <p>Schools attending: 13</p>
+    <p>Location: Old Tappan</p>
+    <p>Judges: 32</p>
+    <button onclick="window.location='edit_tournament.html'">Edit</button>
+
+  </div>
+  <div class="add-tournament-card">
+    +
+  </div>
+</div>
+
     </div>
   );
 }
