e5267bc72b576edbcf41301f8d16ca43674b45f2
Joshua Kim <joskim24@bergen.org>
2024-01-28 13:27:36 -0500

    Some quick changes to inviteadmin page

--- src/components/InviteAdmin.js
+++ src/components/InviteAdmin.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import { useState } from 'react';
 function InviteAdmin() {
     let navigate = useNavigate();
+    const [successSubmit, setSuccess] = useState(false);
     const refresh = () =>{
-    let path = '../settings';
-    navigate(path);
-  }
+        let path = '../settings';
+        navigate(path);
+    }
 
     function handleSubmit(e) {
         // Prevent the browser from reloading the page
@@ -18,28 +19,53 @@ function InviteAdmin() {
         // You can pass formData as a fetch body directly:
         fetch('http://127.0.0.1:5000/save_email', { method: form.method, body: formData });
         window.location.reload(false);
+
       }
 
-
-  return (
-    <div class="tournament-card">
-
-        <h1>Invite Admin</h1>
-        <form method="post" onSubmit={handleSubmit}>
-      <label>
-        Name: <input name="name" />
-      </label>
     
-    <p></p>
-      <label>
-        Email: <input name="email" />
-      </label>
-      
-      <hr />
-      <button type="submit">Submit</button>
-    </form>
-    </div>
-  );
+  if (successSubmit){
+    return (
+        <div class="tournament-card">
+    
+            <h1>Invite Admin</h1>
+            <form method="post" onSubmit={handleSubmit}>
+          <label>
+            Name: <input name="name" />
+          </label>
+        
+        <p></p>
+          <label>
+            Email: <input name="email" />
+          </label>
+          
+          <hr />
+          <button type="submit">Submit</button>
+        </form>
+        <p class="success">Successully added new admin!</p>
+        </div>
+      );
+  } else {
+    return (
+        <div class="tournament-card">
+    
+            <h1>Invite Admin</h1>
+            <form method="post" onSubmit={handleSubmit}>
+          <label>
+            Name: <input name="name" />
+          </label>
+        
+        <p></p>
+          <label>
+            Email: <input name="email" />
+          </label>
+          
+          <hr />
+          <button type="submit">Submit</button>
+        </form>
+        </div>
+      );
+  }
+  
 }
 
 export default InviteAdmin;
